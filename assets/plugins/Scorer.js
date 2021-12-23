390530698ff4356390ae00d6a22debe6306b4588
Ravi Gupta <ravi.art88@gmail.com>
2021-12-23 22:18:56 +0530

    Added Scorer as a HUD element.

--- assets/plugins/Scorer.js
+++ assets/plugins/Scorer.js
@@ -4,19 +4,28 @@ class Scorer extends RenJS.Plugin {
     text = null;
 
     async onCall(params) {
-        if (params.body.includes("REMOVE") && this.scorer){
-            // remove scorer
-            if (params.body.includes("NOFADE")){
-                await this.game.screenEffects.transition.FADEOUT(this.scorer)
-            }
-            this.scorer.destroy();
-            this.scorer = null;
-        }
+      if (params.body == "REMOVE" && this.scorer){
+          // remove scorer
+          await this.game.screenEffects.transition.FADEOUT(this.scorer)
+          this.scorer.destroy();
+          this.scorer = null;
+          this.game.resolveAction();
+          return;
+      }
+      // I want to add different exit fade for the keywords REMOVE WITHNOFADE
+      else if (params.body == "REMOVE WITHNOFADE" && this.scorer){
+          // remove scorer
+          this.scorer.destroy();
+          this.scorer = null;
+          this.game.resolveAction();
+          return;
+      }
         if (this.scorer){
             // Just change the scorer value
             this.text.text = this.game.managers.logic.parseVars(params.body);
         } else {
             this.scorer = this.game.add.sprite(this.game.world.centerX+350, this.game.world.centerY-250, 'scorer'); //loads scorer gui under extras
+            this.game.gui.hud.addChild(this.scorer);
             this.scorer.alpha = 0;
             this.scorer.anchor.set(0.5);
             const style = {...this.game.gui.hud.cHandlers.default.config.text.style};
